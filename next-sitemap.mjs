0b2ed886359f249ed15aa8340d96ecf40cb11815
Robert Samuel White <rsw@rswfire.online>
2025-12-18 18:49:59 -0800

    Updated Sitemap Functionality

--- next-sitemap.mjs
+++ next-sitemap.mjs
@@ -1,12 +1,8 @@
-import type { IConfig } from 'next-sitemap'
-
-const config: IConfig = {
+/** @type {import('next-sitemap').IConfig} */
+export default {
     siteUrl: 'https://oprdvolunteerabuse.org',
     generateRobotsTxt: true,
     outDir: './out',
     changefreq: 'monthly',
     priority: 0.8,
-    exclude: ['/server-sitemap.xml'],
 }
-
-export default config
