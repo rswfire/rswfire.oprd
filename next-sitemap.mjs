6986d6af676980fbb2bff0eaed177e6218bcdc51
Robert Samuel White <rsw@rswfire.online>
2025-12-19 16:33:57 -0800

    Updated Sitemap Configuration

--- next-sitemap.mjs
+++ next-sitemap.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 export default {
     siteUrl: 'https://oprdvolunteerabuse.org',
+    generateIndexSitemap: false,
     generateRobotsTxt: true,
     outDir: './out',
     changefreq: 'monthly',
