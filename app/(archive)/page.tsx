8de51d2029fe352318c7541350761c4d5fe824f3
Robert Samuel White <rsw@rswfire.online>
2025-12-23 16:44:15 -0800

    Updated Index.tsx

--- app/(archive)/page.tsx
+++ app/(archive)/page.tsx
@@ -35,7 +35,7 @@ export default function OverviewPage() {
                 <div className="mx-8 text-base sm:text-lg tracking-tight space-y-4">
                     <div>In early 2025, I served as an unpaid volunteer at <Link href="https://stateparks.oregon.gov/index.cfm?do=park.profile&parkId=95" target="_blank" className="underline text-emerald-800 hover:text-emerald-600">Honeyman State Park</Link>. What began as a routine volunteer assignment became two months of systematic psychological pressure, coercive tactics, and institutional retaliation, followed by dismissal and expulsion from all <Link href="https://www.oregon.gov/oprd/AO/Pages/AU-about.aspx" target="_blank" className="underline text-emerald-800 hover:text-emerald-600">Oregon State Parks</Link>.</div>
                     <div>This archive is not a story about me.</div>
-                    <div>It is a story about <em>them</em> &mdash; the choices they made when given evidence of abuse, when given the opportunity to stop, when given time to self-correct. And every mechanism of accountability they instead used to shield themselves.</div>
+                    <div>It is a story about <em>them</em> &mdash; the choices they made when given evidence of abuse, when given the opportunity to stop, when given time to self-correct. And every mechanism of accountability &mdash; instead used to shield themselves</div>
                     <div className="italic">A system that won't protect unpaid laborers does not deserve them.</div>
                 </div>
 
