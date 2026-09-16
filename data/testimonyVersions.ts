963ddf29da594fa652c980155e425b6e0c627e2c
Robert Samuel White <rsw@rswfire.online>
2026-09-15 21:55:51 -0700

    Testimony: freeze v1.3, start v1.4

--- data/testimonyVersions.ts
+++ data/testimonyVersions.ts
@@ -22,6 +22,12 @@ export interface TestimonyVersion {
 
 export const TESTIMONY_VERSIONS: TestimonyVersion[] = [
     {
+        version: "1.4",
+        date: "September 15, 2026",
+        d: "2026-09-15",
+        note: "",
+    },
+    {
         version: "1.3",
         date: "September 15, 2026",
         d: "2026-09-15",
